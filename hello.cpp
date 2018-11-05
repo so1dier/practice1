30aa80c2ec270c257c79a02dcac3584f0f7b3de9
Daniel Golitsynski <dgolitsynski@emerchants.com.au>
2018-11-05 15:12:42 +1000

    calculate age func

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
 
+
+int CalculateAgeIn100Years(const int years)
+{
+    return years+100;
+}
+
 int main()
 {
     std::cout << "Hello" << std::endl;
+    std::cout << CalculateAgeIn100Years(18) << std::endl;
 }
